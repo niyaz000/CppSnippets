e6e2659f502257772184ef96a3bcd0eafc4887aa
Niyaz K <niyazk2991@gmail.com>
2020-10-01 11:37:42 +0530

    correct typos and spelling mistakes
    correct typos and spelling mistakes

--- concat_vector_of_nums.cc
+++ concat_vector_of_nums.cc
@@ -3,12 +3,12 @@
 #include <vector>
 #include <string>
 
-std::string concat(const std::vector<int> & vec, const std::string & seperator = ",") {
+std::string concat(const std::vector<int> & vec, const std::string & separator = ",") {
     if(vec.empty()) {
         return "";
     }
- auto concant_elements = [seperator](std::string a, int arg) -> std::string {
-  return std::move(a) + seperator + std::to_string(arg);
+ auto concat_elements = [separator](std::string a, int arg) -> std::string {
+  return std::move(a) + separator + std::to_string(arg);
  };
- return std::accumulate(std::next(vec.cbegin()), vec.cend(), std::to_string(vec[0]), concant_elements);
+ return std::accumulate(std::next(vec.cbegin()), vec.cend(), std::to_string(vec[0]), concat_elements);
 }
